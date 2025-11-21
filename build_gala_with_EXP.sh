5f9d3fcf15cb41e0a4c07c6e32833ca2af1b554f
Andrea Sante <asante@worker6098.cm.cluster>
2025-11-21 11:47:53 -0500

    Added flag to EXP make call and running gala tests benchmarks after installation

--- build_gala_with_EXP.sh
+++ build_gala_with_EXP.sh
@@ -12,6 +12,7 @@ git clone --recursive https://github.com/EXP-code/EXP.git -b devel
 cd EXP
 cmake -G Ninja -B build -DCMAKE_INSTALL_RPATH=$PWD/install/lib  \
  --install-prefix $PWD/install \
+ -DCMAKE_BUILD_TYPE=Release \
  -DBUILD_DOCS=YES  \
  -DENABLE_PYEXP=YES \
  -DENABLE_NBODY=YES \
@@ -46,3 +47,6 @@ export GALA_EXP_PREFIX=$EXP_path/install
 
 uv pip install -ve .
 
+# Run benchmark tests
+uv sync --all-extras
+uv run pytest tests/benchmarks -k TestEXPTimeInterpBenchmark --codspeed --durations=0
